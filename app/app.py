c24b2690e4fa13f957230e6e54392d39f46c835b
vishal-goognu44 <vishal@goognu.com>
2026-03-06 21:32:28 +0000

    c6

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask DevOps Pipeline!"
+    return "Hello New Image to be created"
 
 @app.route("/health")
 def health():
